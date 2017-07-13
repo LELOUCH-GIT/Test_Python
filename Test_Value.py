38b3e31bb70cd04dcd453a8135290fb14fceb76d
LELOUCH <15804536790@163.com>
2017-07-13 17:55:55 +0800

    Test Value over

--- Test_Value.py
+++ Test_Value.py
@@ -1,8 +1,31 @@
-a = b = c = 1, 2, 'jaune'
+a = b = c = [1, 2, 'jaune']
 d, e, f = 1, 2, 'jaune'
 print(a)
 print(b)
 print(c)
-print("%d %d %s" % (d, e, f) , end='')
+print("%d %d %s" % (d, e, f))
+print(id(a))
+print(id(b))
+a[0]=3
+print(a)
+print(b)
+str = 'I love my sister '
+print(str[1:10])
+print(str * 2)
+print(a + b)
+# 下面是字典
+# 列表【】 元祖（） 字典{}
+dict = {}
+dict['one'] = "This is one"
+dict[2]="This is two"
+tinydict = {'name': 'john', 'code': 6754, 'dept': 'sales'}
+print(dict['one'])
+print(dict[2])
+print(tinydict)
+print(tinydict.keys())
+print(tinydict.values())
+x = 1, 2, 3
+print(type(x))
+
 
 
