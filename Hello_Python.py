bca0072381812d967df55a18192e34cff5773096
LELOUCH <15804536790@163.com>
2017-07-13 12:58:55 +0800

    change hello_python

--- Hello_Python.py
+++ Hello_Python.py
@@ -1,2 +1,8 @@
 # 注释测试1
-print('hello python') # 注释测试2
+print('你好 python') # 注释测试2
+word1 = '23333333333333'
+word2 = '''6666666
+66666666''' # 段落表示  允许有回车的
+word3 = word1 + \
+        word2
+print(word3) #字符串测试
