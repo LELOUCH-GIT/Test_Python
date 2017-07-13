20b599d5094443676e484201eb2c7cb2931eca99
LELOUCH <15804536790@163.com>
2017-07-13 15:43:31 +0800

    if 测试

--- Hello_Python.py
+++ Hello_Python.py
@@ -1,4 +1,5 @@
 # 注释测试1
+import sys
 print('你好 python') # 注释测试2
 word1 = '23333333333333'
 word2 = '''6666666
@@ -6,3 +7,19 @@ word2 = '''6666666
 word3 = word1 + \
         word2
 print(word3) #字符串测试
+day = {'1', '2',
+       '3', '4'}
+'''
+这个符号居然可以表示多行注释
+简直不敢相信
+'''
+# 下面是输出还行与不换行的测试
+x = 'sb'; y = 'hehe'
+print(x,end=' and ');print(y)
+# 下面是if测试
+judge = True
+if judge:
+    print('True')
+else:
+    print('False')
+
