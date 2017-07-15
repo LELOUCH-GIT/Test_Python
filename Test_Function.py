93c8f5f05d74c395ba3dfba027c13c777dee92fe
LELOUCH <15804536790@163.com>
2017-07-15 13:12:32 +0800

    qw

--- Test_Function.py
+++ Test_Function.py
@@ -1,10 +1,20 @@
-import unittest
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def printme(str):
+    print(str)
+    return
+def function(argl, *vartuple ):
+    print(argl)
+    for var in vartuple:
+        print(var)
+    return
+sum = lambda arg1, arg2: arg1 + arg2
+print(sum(1, 2))
+print(sum('a', 'b'))
+printme('hello')
+function(10)
+function(10,20,30)
+globalvar = 0
+def set_global_var():
+    global globalvar
+    globalvar = 1
+set_global_var()
+print(globalvar)
